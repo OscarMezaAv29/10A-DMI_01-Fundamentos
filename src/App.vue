992afd31ab37a09385bc2abfc748f067d25a2980
OscarMezaAv29 <a3519110251@alumno.uttehuacan.edu.mx>
2022-09-12 13:22:47 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,64 @@
+<script>
+  export default {
+    data: () => ({
+      contador: 10,
+    }),
+    methods: {
+      saludar(){
+        console.log("Hola 10A");
+      },
+      incrementar(){
+        this.contador++;
+      },
+      decrementar(){
+        this.contador--;
+      },
+      resetear(){
+        this.contador = 10;
+      },
+    }
+  };
+</script>
+
 <template>
   <div class="alert alert-primary" role="alert">
     Oscar Alejandro Meza Avendaño | 10° A
   </div>
   <br>
-  <img class="logo" src="./assets/logo.svg" alt="Logo VUe" >
-  <h1>Hola desde VueJS 3</h1>
-  <hr>
-  <h2 class="alert alert-success">Contador 0</h2>
-  <button class="btn btn-success">+1</button>
-  <button class="btn btn-success" variant="info">-1</button>
-  <button class="reset btn btn-danger">Reset</button>
+  <img class="logo" src="./assets/logo.svg" alt="Logo VUe" width="125" height="125">
+  <div class="container">
+    <small><h1>Hola desde VueJS 3</h1></small>
+    <hr>
+    <h2>Contador {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="resetear()">Reset</button>
+  </div>
 </template>
 
 <style scoped>
-  h1 {
-    color: red;
-    font-family: 'Montserrat', sans-serif;
+  .container {
+    text-align: center;
   }
 
+  small {
+    color: #666;
+  }
+
+  h1 {
+    font-weight: bolder;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  button {
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0px 8px;
+  }
+  /*
   #app .alert  {
     width: 100%;
     grid-column-start: 1;
@@ -43,5 +84,5 @@
     grid-column-start: 1;
     grid-column-end: 3;
     text-align: center;
-  }
+  }*/
 </style>
